9bb7000cc5be4a96bf16e5aacb338d23bb973892
Thendior <161wit12@o365.htl-leoben.at>
2018-05-30 05:41:44 -0700

    schleifen  und Würfel

---
+++ Schleifen.py
@@ -0,0 +1,9 @@
+# Schleifen
+from random import * 
+
+anz = 10
+for i in range (0, anz):
+    print (i, end="...")
+print("")
+print(random())
+print(randint(1,6))
---
+++ wuerfel.py
@@ -0,0 +1,4 @@
+# wuerfel.py
+
+# 100 mal würfel simulieren
+#anzahl von 1 2 3 4 5 6 ausgeben
